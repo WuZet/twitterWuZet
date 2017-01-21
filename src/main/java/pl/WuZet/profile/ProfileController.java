b468f6a06331e5b2d410523c29bd0e91125c4a15
WuZet <wieslawa.zaborowska@wp.eu>
2017-01-21 20:33:08 +0100

    added DateFormatter

--- src/main/java/pl/WuZet/profile/ProfileController.java
+++ src/main/java/pl/WuZet/profile/ProfileController.java
@@ -1,8 +1,12 @@
 package pl.WuZet.profile;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.WuZet.USLocalDateFormatter.USLocalDateFormatter;
+
+import java.util.Locale;
 
 @Controller
 public class ProfileController {
@@ -10,6 +14,11 @@ public class ProfileController {
     public String displayProfile(ProfileForm profileForm) {
 
         return "profile/profilePage";
+    }
+
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+        return USLocalDateFormatter.getPattern(locale);
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
