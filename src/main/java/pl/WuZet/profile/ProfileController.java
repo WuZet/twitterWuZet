2bf991466b1488e992a817ac683ef0cee8d4cd57
WuZet <wieslawa.zaborowska@wp.eu>
2017-01-21 21:12:18 +0100

    added validation form

--- src/main/java/pl/WuZet/profile/ProfileController.java
+++ src/main/java/pl/WuZet/profile/ProfileController.java
@@ -1,11 +1,13 @@
 package pl.WuZet.profile;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.WuZet.USLocalDateFormatter.USLocalDateFormatter;
 
+import javax.validation.Valid;
 import java.util.Locale;
 
 @Controller
@@ -22,7 +24,10 @@ public class ProfileController {
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(ProfileForm profileForm) {
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "profile/profilePage";
+        }
         System.out.println("pomyślnie zapisany profil " + profileForm);
         return "redirect:/profile";
     }
--- src/main/java/pl/WuZet/profile/ProfileForm.java
+++ src/main/java/pl/WuZet/profile/ProfileForm.java
@@ -1,13 +1,25 @@
 package pl.WuZet.profile;
 
+import com.sun.istack.internal.NotNull;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.Size;
+
 public class ProfileForm {
+    @Size(min = 2)
     private String twitterHandle;
+    @Email
+    @NotEmpty
     private String email;
+    @NotNull
     private LocalDate birthDate;
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 
     public String getTwitterHandle() {
